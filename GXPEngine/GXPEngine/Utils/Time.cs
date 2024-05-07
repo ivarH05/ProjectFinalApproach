0261638176cf8eef6d41d344604394b414222b6d
ivarH05 <119761956+ivarH05@users.noreply.github.com>
2024-05-07 11:26:03 +0200

    Started on the physics manager

--- GXPEngine/GXPEngine/Utils/Time.cs
+++ GXPEngine/GXPEngine/Utils/Time.cs
@@ -8,9 +8,13 @@ namespace GXPEngine
 	/// </summary>
 	public class Time
 	{
-		private static int previousTime;
+		public static float timeStep = 0.02f;
 
-		static Time() {
+        private static int previousTime;
+
+        private static float previousTimeSeconds;
+
+        static Time() {
 		}
 		
 		/// <summary>
@@ -19,34 +23,47 @@ namespace GXPEngine
 		public static int now {
 			get { return System.Environment.TickCount; }
 		}
-		
-		/// <summary>
-		/// Returns this time in milliseconds since the program started		
-		/// </summary>
-		/// <value>
-		/// The time.
-		/// </value>
-		public static int time {
-			get { return (int)(OpenGL.GL.glfwGetTime()*1000); }
-		}
-		
-		/// <summary>
-		/// Returns the time in milliseconds that has passed since the previous frame
-		/// </summary>
-		/// <value>
-		/// The delta time.
-		/// </value>
-		private static int previousFrameTime;
-		public static int deltaTime {
-			get { 
-				return previousFrameTime; 
-			}
-		}
 
-		internal static void newFrame() {
+        /// <summary>
+        /// Returns this time in milliseconds since the program started		
+        /// </summary>
+        /// <value>
+        /// The time.
+        /// </value>
+        public static int time
+        {
+            get { return (int)(OpenGL.GL.glfwGetTime() * 1000); }
+        }
+        public static float timeSeconds
+        {
+            get { return (float)OpenGL.GL.glfwGetTime(); }
+        }
+
+        /// <summary>
+        /// Returns the time in milliseconds that has passed since the previous frame
+        /// </summary>
+        /// <value>
+        /// The delta time.
+        /// </value>
+        private static int previousFrameTime;
+        public static int deltaTime
+        {
+            get
+            {
+                return previousFrameTime;
+            }
+        }
+
+
+        public static float DeltaSeconds { get; private set; }
+
+        internal static void newFrame() {
 			previousFrameTime = time - previousTime;
 			previousTime = time;
-		}
+            DeltaSeconds = timeSeconds - previousTimeSeconds;
+            previousTimeSeconds = timeSeconds;
+
+        }
 	}
 }
 
---
+++ GXPEngine/Managers/PhysicsManager.cs
@@ -0,0 +1,24 @@
+﻿using GXPEngine.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine.Managers
+{
+    public static class PhysicsManager
+    {
+        private static List<Rigidbody> _bodies = new List<Rigidbody>();
+        private static float updateTimer = 0;
+
+        public static void PhysicsUpdate()
+        {
+            updateTimer += Time.DeltaSeconds;
+            while (updateTimer > Time.timeStep)
+            {
+                updateTimer -= Time.timeStep;
+            }
+        }
+    }
+}
