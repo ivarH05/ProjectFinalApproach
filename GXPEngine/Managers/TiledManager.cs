dff144966f9ccac8bd78425fe7c765cc9f64d06e
AkaruiAnims <60879671+AkaruiAnims@users.noreply.github.com>
2024-05-07 11:54:44 +0200

    tiled manager added

---
+++ GXPEngine/Managers/TiledManager.cs
@@ -0,0 +1,15 @@
+using GXPEngine.Utility;
+using System;
+
+
+namespace GXPEngine.Managers
+{
+    class TiledManager
+    {
+        public static void LoadTiledMap(string path)
+        {
+            Console.WriteLine("Loading Tiled Map: " + path);
+        }
+    }
+
+}
