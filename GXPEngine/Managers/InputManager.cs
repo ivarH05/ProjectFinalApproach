0ea68c5486a7f2742bd93c7a929465e8927c9905
Jerpharney <jerpharney@gmail.com>
2024-05-08 08:46:59 +0200

    Update InputManager.cs

--- GXPEngine/Managers/InputManager.cs
+++ GXPEngine/Managers/InputManager.cs
@@ -9,18 +9,104 @@ namespace GXPEngine.Managers
 {
     public class InputManager 
     {
-        int shootKey = Key.SPACE;           // also UI select
-        int leftBumper = Key.LEFT;          // also UI left
-        int rightBumper = Key.RIGHT;        // also UI right
-        bool bothBumpers = false;           // use when both bumpers are pressed
+        public const int shootKey = Key.SPACE;           // also UI select
+        public const int leftBumper = Key.LEFT;          // also UI left
+        public const int rightBumper = Key.RIGHT;        // also UI right
+        const int debugToggle = Key.F1;
+        Dictionary<int, bool> isHeld;
 
         public InputManager () 
         {
+            isHeld = new Dictionary<int, bool>()
+            {
+                {shootKey, Input.GetKey(shootKey)},
+                {leftBumper, Input.GetKey(leftBumper)},
+                {rightBumper, Input.GetKey(rightBumper)}
+            };
         }
 
 
+        void UpdateHeldKeys()
+        {
+            isHeld[shootKey] = Input.GetKey(shootKey);
+            isHeld[leftBumper] = Input.GetKey(leftBumper);
+            isHeld[rightBumper] = Input.GetKey(rightBumper);
+        }
+
+        public bool ToggleDebugMode()
+        {
+            return Input.GetKeyDown(debugToggle);
+        }
+
+        public bool IsHeld(int key)
+        {
+            return isHeld[key];
+        }
+
+        public bool IsShot() 
+        {
+            return Input.GetKeyDown(shootKey);
+        }
+
+        public bool LeftBumperPressed()
+        {
+
+            return Input.GetKeyDown(leftBumper);
+        } 
+
+        public bool RightBumperPressed()
+        {
+            return Input.GetKeyDown(rightBumper);
+        }
+
+        public bool BothBumpersPressed()
+        {
+            return Input.GetKey(leftBumper) && Input.GetKey(rightBumper);
+        }
+
+        void DebugInput()
+        {
+            Console.WriteLine( "Shoot key is pressed:" + IsShot() + "\n" +
+                                "Left bumper is pressed:" + LeftBumperPressed() + "\n" +
+                                "Right bumper is pressed:" + RightBumperPressed() + "\n" +
+                                "Both bumpers are pressed:" + BothBumpersPressed());
+        }
+
+        void DebugInput(int keyName, bool heldMode = false)
+        {
+            if ( !heldMode )
+            {
+                switch (keyName)
+                {
+                    case shootKey:
+                        Console.WriteLine( "Shoot key is pressed:" + IsShot());
+                        break;
+                    case leftBumper:
+                        Console.WriteLine( "Left bumper is pressed:" + LeftBumperPressed());
+                        break;
+                    case rightBumper:
+                        Console.WriteLine( "Right bumper is pressed:" + RightBumperPressed());
+                        break;
+                }
+            } else {
+                switch (keyName)
+                {
+                    case shootKey:
+                        Console.WriteLine( "Shoot key is held:" + IsHeld(shootKey));
+                        break;
+                    case leftBumper:
+                        Console.WriteLine( "Left bumper is held:" + IsHeld(leftBumper));
+                        break;
+                    case rightBumper:
+                        Console.WriteLine( "Right bumper is held:" + IsHeld(rightBumper));
+                        break;
+                }
+            }
+        }
+
         public void Update()
         {
+            UpdateHeldKeys();
         }
     }
 }
