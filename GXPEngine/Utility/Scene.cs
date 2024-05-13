1fd1adf3949cd1419b070e0a476e3752230173b7
Jerpharney <jerpharney@gmail.com>
2024-05-13 15:03:53 +0200

    new assets

--- GXPEngine/Utility/Scene.cs
+++ GXPEngine/Utility/Scene.cs
@@ -63,7 +63,7 @@ namespace GXPEngine
             AddChild(debugger);
             PhysicsManager.setup();
             
-            // tiledManager.LoadTiledMap("testmap.tmx");       //Uncomment this line to load a tiled map
+            tiledManager.LoadTiledMap("map1/map1.tmx");       //Uncomment this line to load a tiled map
         }
 
         void Update()
