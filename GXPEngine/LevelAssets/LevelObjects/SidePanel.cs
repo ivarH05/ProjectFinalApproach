a6a34030b8cdef86572c810b038714011231123a
Jerpharney <jerpharney@gmail.com>
2024-05-14 13:59:21 +0200

    Update SidePanel.cs

--- GXPEngine/LevelAssets/LevelObjects/SidePanel.cs
+++ GXPEngine/LevelAssets/LevelObjects/SidePanel.cs
@@ -7,12 +7,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
 using TiledMapParser;
+using System.Dynamic;
 
 namespace GXPEngine
 {
     public class SidePanel : Sprite
     {
         public static Vec2[] coords;
+        object[] panelChildren;
 
         public SidePanel() : base("square.png", false)
         {
@@ -25,5 +27,12 @@ namespace GXPEngine
             alpha = 0f;
             coords = new Vec2[]{ new Vec2(obj.X, obj.Y), new Vec2(obj.Width, obj.Height)  };
         }
+
+        void GetPanelChildren()
+        {
+            // get all children of the panel
+            
+        }
+
     }
 }
